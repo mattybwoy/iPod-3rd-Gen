d8c9be27e589f52704c7ef520a9b4259bf87118d
Matthew Lock <matthew_lock20@hotmail.com>
2023-09-14 19:29:21 +0100

    Initial Commit

---
+++ iPod 3rd Gen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iPod 3rd Gen
+//
+//  Created by Matthew Lock on 14/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iPod 3rd GenUITests/iPod_3rd_GenUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iPod_3rd_GenUITestsLaunchTests.swift
+//  iPod 3rd GenUITests
+//
+//  Created by Matthew Lock on 14/09/2023.
+//
+
+import XCTest
+
+final class iPod_3rd_GenUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
