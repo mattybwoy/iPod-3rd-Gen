746b2c631dedce5b5b5303e1cc817768f8d3120a
Matthew Lock <matthew_lock20@hotmail.com>
2023-09-25 21:16:57 +0100

    added button Icons

--- iPod 3rd Gen/ViewControllers/MainViewController.swift
+++ iPod 3rd Gen/ViewControllers/MainViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let backButton = UIButton()
+    let menuButton = UIButton()
+    let playButton = UIButton()
+    let forwardButton = UIButton()
+    
     override func viewDidLoad() {
         view.backgroundColor = .black
-
+        makeButtonRounded()
+        
         let iPodOutlineView = RoundedRectangle()
         self.view.addSubview(iPodOutlineView)
         iPodOutlineView.translatesAutoresizingMaskIntoConstraints = false
@@ -34,7 +40,7 @@ class ViewController: UIViewController {
         
         let clickWheelView = ClickWheel()
         self.view.addSubview(clickWheelView)
-            clickWheelView.translatesAutoresizingMaskIntoConstraints = false
+        clickWheelView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             clickWheelView.leadingAnchor.constraint(equalTo: iPodOutlineView.leadingAnchor, constant: 60),
             clickWheelView.trailingAnchor.constraint(equalTo: iPodOutlineView.trailingAnchor, constant: -60),
@@ -44,7 +50,7 @@ class ViewController: UIViewController {
         
         let clickWheelButtonView = ClickWheelButton()
         self.view.addSubview(clickWheelButtonView)
-            clickWheelButtonView.translatesAutoresizingMaskIntoConstraints = false
+        clickWheelButtonView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             clickWheelButtonView.leadingAnchor.constraint(equalTo: clickWheelView.leadingAnchor, constant: 82),
             clickWheelButtonView.trailingAnchor.constraint(equalTo: clickWheelView.trailingAnchor, constant: -82),
@@ -53,16 +59,12 @@ class ViewController: UIViewController {
             clickWheelButtonView.heightAnchor.constraint(equalToConstant: 105),
         ])
         
-        let backButton = ActionButton()
-        self.view.addSubview(backButton)
-        let menuButton = ActionButton()
         self.view.addSubview(menuButton)
-        let playButton = ActionButton()
         self.view.addSubview(playButton)
-        let forwardButton = ActionButton()
         self.view.addSubview(forwardButton)
+        self.view.addSubview(backButton)
         
-            backButton.translatesAutoresizingMaskIntoConstraints = false
+        backButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             backButton.leadingAnchor.constraint(equalTo: iPodOutlineView.leadingAnchor, constant: 26),
             backButton.topAnchor.constraint(equalTo: lcdView.bottomAnchor, constant: 40),
@@ -70,7 +72,7 @@ class ViewController: UIViewController {
             backButton.widthAnchor.constraint(equalToConstant: 70)
         ])
         
-            menuButton.translatesAutoresizingMaskIntoConstraints = false
+        menuButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             menuButton.leadingAnchor.constraint(equalTo: backButton.trailingAnchor, constant: 20),
             menuButton.topAnchor.constraint(equalTo: lcdView.bottomAnchor, constant: 40),
@@ -78,7 +80,7 @@ class ViewController: UIViewController {
             menuButton.widthAnchor.constraint(equalToConstant: 70)
         ])
         
-            playButton.translatesAutoresizingMaskIntoConstraints = false
+        playButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             playButton.leadingAnchor.constraint(equalTo: menuButton.trailingAnchor, constant: 20),
             playButton.topAnchor.constraint(equalTo: lcdView.bottomAnchor, constant: 40),
@@ -86,7 +88,7 @@ class ViewController: UIViewController {
             playButton.widthAnchor.constraint(equalToConstant: 70)
         ])
         
-            forwardButton.translatesAutoresizingMaskIntoConstraints = false
+        forwardButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             forwardButton.trailingAnchor.constraint(equalTo: iPodOutlineView.trailingAnchor, constant: -26),
             forwardButton.topAnchor.constraint(equalTo: lcdView.bottomAnchor, constant: 40),
@@ -94,6 +96,46 @@ class ViewController: UIViewController {
             forwardButton.widthAnchor.constraint(equalToConstant: 70)
         ])
     }
-
+    
+    private func makeButtonRounded() {
+        backButton.layer.borderColor = UIColor(named: "Shadow")?.cgColor
+        backButton.layer.borderWidth = 1.5
+        backButton.layer.masksToBounds = true
+        var backConfig = UIButton.Configuration.plain()
+        backConfig.image = UIImage(systemName: "backward.end.alt.fill", withConfiguration: UIImage.SymbolConfiguration(scale: .small))
+        backButton.configuration = backConfig
+        backButton.tintColor = UIColor(named: "ButtonStandard")
+        
+        menuButton.layer.borderColor = UIColor(named: "Shadow")?.cgColor
+        menuButton.layer.borderWidth = 1.5
+        menuButton.layer.masksToBounds = true
+        menuButton.setTitle("MENU", for: .normal)
+        menuButton.titleLabel?.font = .systemFont(ofSize: 13.0, weight: .bold)
+        menuButton.setTitleColor(UIColor(named: "ButtonStandard"), for: .normal)
+        menuButton.tintColor = UIColor(named: "ButtonStandard")
+        
+        playButton.layer.borderColor = UIColor(named: "Shadow")?.cgColor
+        playButton.layer.borderWidth = 1.5
+        playButton.layer.masksToBounds = true
+        var playConfig = UIButton.Configuration.plain()
+        playConfig.image = UIImage(systemName: "playpause.fill", withConfiguration: UIImage.SymbolConfiguration(scale: .small))
+        playButton.configuration = playConfig
+        playButton.tintColor = UIColor(named: "ButtonStandard")
+        
+        forwardButton.layer.borderColor = UIColor(named: "Shadow")?.cgColor
+        forwardButton.layer.borderWidth = 1.5
+        forwardButton.layer.masksToBounds = true
+        var forwardConfig = UIButton.Configuration.plain()
+        forwardConfig.image = UIImage(systemName: "forward.end.alt.fill", withConfiguration: UIImage.SymbolConfiguration(scale: .small))
+        forwardButton.configuration = forwardConfig
+        forwardButton.tintColor = UIColor(named: "ButtonStandard")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        backButton.layer.cornerRadius = 0.5 * backButton.bounds.size.width
+        menuButton.layer.cornerRadius = 0.5 * menuButton.bounds.size.width
+        playButton.layer.cornerRadius = 0.5 * playButton.bounds.size.width
+        forwardButton.layer.cornerRadius = 0.5 * forwardButton.bounds.size.width
+    }
 }
 
